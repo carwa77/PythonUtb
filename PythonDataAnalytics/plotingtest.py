721c24fce58d6559de100d782694e9c541e334e4
Carl Wallin <clwn77@gmail.com>
2020-11-22 19:06:35 +0100

    Add more files

---
+++ PythonDataAnalytics/plotingtest.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+plt.figure(1)
+plt.plot([1,2,3,4])
+
+plt.figure(2)
+plt.title('Min graf')
+plt.plot([1,2,3,4],[1,4,9,10],'ro')
+plt.show()
---
+++ Udemy/test.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import tkinter as tk
+
+u = 5+9
+print(u)
